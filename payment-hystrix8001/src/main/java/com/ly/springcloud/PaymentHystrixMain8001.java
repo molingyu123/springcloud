1424ed864fb090cd7ff55d6b9fcf22148acb6d28
MOLY <815709965@qq.com>
2020-07-04 22:30:36 +0800

    断路器服务降级配置

--- payment-hystrix8001/src/main/java/com/ly/springcloud/PaymentHystrixMain8001.java
+++ payment-hystrix8001/src/main/java/com/ly/springcloud/PaymentHystrixMain8001.java
@@ -2,6 +2,7 @@ package com.ly.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker// 激活服务熔断
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
--- payment-hystrix8001/src/main/java/com/ly/springcloud/service/PaymentService.java
+++ payment-hystrix8001/src/main/java/com/ly/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.ly.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -16,13 +18,21 @@ public class PaymentService {
        return "线程池:"+Thread.currentThread().getName()+" paymentInfoOk,id:"+id+"\t"+"haha";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeoutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1000")
+    })
     public String paymentInfoTimeout (Integer id){
-        int timeNumber =3;
+        int timeNumber =2;
         try{
             TimeUnit.SECONDS.sleep(timeNumber);
         }catch (InterruptedException e){
             e.printStackTrace();
         }
         return "线程池:"+Thread.currentThread().getName()+" paymentInfoTimeout,id:"+id+"\t"+"耗时"+timeNumber+"秒钟";
+    }
+
+    public String paymentInfoTimeoutHandler (Integer id){
+        return "线程池:"+Thread.currentThread().getName()+" paymentInfoTimeoutHanlder,id:"+id+"\t"+"请稍后再试";
+
     }
 }
