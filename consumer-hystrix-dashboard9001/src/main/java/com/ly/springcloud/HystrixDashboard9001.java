f743770de8fc723b5c3bc04df0a90e70d23362e9
MOLY <815709965@qq.com>
2020-07-07 21:12:42 +0800

    断路器可视化监控配置

---
+++ consumer-hystrix-dashboard9001/src/main/java/com/ly/springcloud/HystrixDashboard9001.java
@@ -0,0 +1,14 @@
+package com.ly.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+
+@SpringBootApplication
+@EnableHystrixDashboard// 开启图形化功能
+public class HystrixDashboard9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboard9001.class,args);
+    }
+}
--- payment-hystrix8001/src/main/java/com/ly/springcloud/PaymentHystrixMain8001.java
+++ payment-hystrix8001/src/main/java/com/ly/springcloud/PaymentHystrixMain8001.java
@@ -1,9 +1,13 @@
 package com.ly.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author mly
@@ -14,5 +18,21 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
+
+    /**
+     * 此配置为了服务监控而配置，与服务器本身无关，springcloud 升级后留下的坑
+     * ServletRegistrationBean 因为springboot 的默认路径不是hystrix.stream
+     * 因此需要配置以下的对象
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet hystrixMetricsStreamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(hystrixMetricsStreamServlet);
+        servletRegistrationBean.setLoadOnStartup(1);
+        servletRegistrationBean.addUrlMappings("/hystrix.stream");
+        servletRegistrationBean.setName("HystrixMetricsStreamServlet");
+        return servletRegistrationBean;
     }
 }
