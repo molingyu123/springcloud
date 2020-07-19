938acb931a5b23b0e5da10c9b49f44a16426d5b7
MOLY <815709965@qq.com>
2020-07-19 11:21:56 +0800

    spring cloud config client 配置

--- cloud-config-client-3345/src/main/java/com/ly/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3345/src/main/java/com/ly/springcloud/controller/ConfigClientController.java
@@ -2,16 +2,19 @@ package com.ly.springcloud.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author mly
+ * @RefreshScope// 刷新注解
  */
+@RefreshScope
 @RestController
 public class ConfigClientController {
 
-    @Value("${spring.cloud.config}")
+    @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/getInfo")
